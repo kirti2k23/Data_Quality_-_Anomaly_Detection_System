d23a8d2ca204f12c1bc077b534717bf09e30f044
Kirti Verma <Kv.edu14@gmail.com>
2026-01-03 16:49:46 +0530

    dataset downloaded

--- src/Data_quality_and_anomaly_detection/logger.py
+++ src/Data_quality_and_anomaly_detection/logger.py
@@ -7,7 +7,7 @@ from datetime import datetime
 This folder will do logging and save log files in Logs folder
 """
 log_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),"Logs")
-log_file = f"log_file{datetime.now().strftime('%Y%m%%d_%H%M%S')}.log"
+log_file = f"log_file{datetime.now().strftime('%Y%m%d_%H%M%S')}.log"
 os.makedirs(log_folder,exist_ok=True)
 
 Log_file_path = os.path.join(log_folder,log_file)
---
+++ src/Data_quality_and_anomaly_detection/utils/common.py
@@ -0,0 +1,16 @@
+import os
+from src.Data_quality_and_anomaly_detection.logger import logging
+from src.Data_quality_and_anomaly_detection.exception import MycustomException
+
+
+
+
+def create_artifacts_dir(data_path: str)-> str:
+    try:
+        folder = "artifacts"
+        artifacts_path = os.path.join(folder,data_path)
+        os.makedirs(artifacts_path,exist_ok=True)
+        logging.info(f"{folder}/{data_path} is created successfully")
+    except Exception as e:
+        raise MycustomException(e)
+    return artifacts_path
